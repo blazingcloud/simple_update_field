266266155d9b0c7525fd85d02e6b11f926eac085
Hollin Wilkins <chromaticbum@gmail.com>
2012-05-14 14:47:19 -0700

    only one input at a time - multi input attempt triggers complete (commit) - works across multiple invocations of SimpleUpdateField

--- vendor/assets/javascripts/simple_update_field.js
+++ vendor/assets/javascripts/simple_update_field.js
@@ -1,5 +1,9 @@
 //1
 SimpleUpdateField = function(selector, options) {
+  SimpleUpdateField.current_active = ( SimpleUpdateField.current_active || null )
+  SimpleUpdateField.TAB_KEY   = 9   // tab is   #9
+  SimpleUpdateField.ENTER_KEY = 13  // enter is #13
+  SimpleUpdateField.ESC_KEY   = 27  // enter is #13
   var self = this
   /*
    * This is used internally to track what key has been
@@ -36,8 +40,17 @@ SimpleUpdateField = function(selector, options) {
   }
 
   var begin_edit_event = function(event) {
-      var clicked_node = $(this);
-      var input_node = create_input_from(clicked_node);
+      var clicked_node = $(this)
+
+      if(SimpleUpdateField.current_active) {
+        SimpleUpdateField.current_active.removeClass('suf-editable-active')
+        SimpleUpdateField.current_active.children('input').trigger('complete.editable')
+      }
+
+      SimpleUpdateField.current_active = clicked_node
+
+      var input_node = create_input_from(clicked_node)
+      clicked_node.addClass('suf-editable-active')
       clicked_node.text("")
       clicked_node.append(input_node)
       install_edit_complete_notions(input_node)
@@ -144,7 +157,6 @@ SimpleUpdateField = function(selector, options) {
   }
 
   var install_edit_notions = function(selector) {
-
     $(selector).bind('click.editable',begin_edit_event)
     $(selector).bind('mouseover.editable',function() {
       $(this).addClass('editable-hover')
@@ -203,6 +215,7 @@ SimpleUpdateField = function(selector, options) {
   }
 
   var install = function() {
+    $(selector).addClass('suf-editable')
     install_edit_notions(selector)
     annotate_editable_with_position(selector)
     annotate_with_resource_errors(selector)
@@ -210,6 +223,3 @@ SimpleUpdateField = function(selector, options) {
   }()
   return self;
 }
-SimpleUpdateField.TAB_KEY   = 9   // tab is   #9
-SimpleUpdateField.ENTER_KEY = 13  // enter is #13
-SimpleUpdateField.ESC_KEY   = 27  // enter is #13
